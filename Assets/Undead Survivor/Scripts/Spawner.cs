4e946f2ff470c45f2b13b85f3e92d0a44014cf09
MINUK <zoow2@naver.com>
2023-03-20 19:52:59 +0900

    hotcontrol 오류 해결 및 spawnPoint를 이용한 enemy spawn 생성

--- Assets/Undead Survivor/Scripts/Spawner.cs
+++ Assets/Undead Survivor/Scripts/Spawner.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    
+    public Transform[] spawnPoint;
+
+    float timer;
+
+    void Awake()
+    {
+        spawnPoint = GetComponentsInChildren<Transform>();
+    }
+
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
-        {
-            GameManager.Instance.pool.Get(1);
+        timer += Time.deltaTime;
+
+        if(timer > 0.2f)
+        {          
+            timer = 0;
+            Spawn();
         }
+    }
+
+    void Spawn()
+    {
+        GameObject enemy = GameManager.Instance.pool.Get(Random.Range(0, 2));
+        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
     }
 }
